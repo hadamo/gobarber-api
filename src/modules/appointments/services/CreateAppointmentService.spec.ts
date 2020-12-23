820b075855ad3cfb3b5cfe570ba0cd386a4367e6
hadamo <hadamosieg@gmail.com>
2020-12-23 20:53:51 -0300

    Testes com jest

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +18,23 @@ describe('CreateAppointment', () => {
         expect(appointment.provider_id).toBe('123123123');
     });
 
-    // it('should not be able to create a new appointment on the same time', () => {
-    //     expect()
-    // });
+    it('should not be able to create a new appointment on the same time', async () => {
+        const fakeAppointmentRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentRepository,
+        );
+
+        const appointmentDate = new Date(2020, 11, 23, 11);
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123123123',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '123123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });
